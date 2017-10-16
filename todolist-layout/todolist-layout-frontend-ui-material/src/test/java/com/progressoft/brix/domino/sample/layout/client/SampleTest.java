62452cad25fd68c7836aa3f68e373731c52852fb
Ahmad Bawaneh <akabme@gmail.com>
2017-10-16 22:39:29 +0300

    try some gwt test cases

---
+++ todolist-layout/todolist-layout-frontend-ui-material/src/test/java/com/progressoft/brix/domino/sample/layout/client/SampleTest.java
@@ -0,0 +1,14 @@
+package com.progressoft.brix.domino.sample.layout.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class SampleTest extends GWTTestCase {
+    @Override
+    public String getModuleName() {
+        return "com.progressoft.brix.domino.sample.layout.LayoutUIClientModule";
+    }
+
+    public void test(){
+        assertTrue(true);
+    }
+}
